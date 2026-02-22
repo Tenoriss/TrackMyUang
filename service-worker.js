60e374ca64deda821c69b27deb8688404aa62568
AtennnDevvStressstttt <fstanlye@gmail.com>
2026-02-23 06:39:30 +0700

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "mykeuangan-v1";
+
+const urlsToCache = [
+  "index.html",
+  "logo.jpeg",
+  "click.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
